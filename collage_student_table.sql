17c46c6f101cd7985375d4581849a62dfa57caba
Lakshbethariya <Lakshbethariya03@gmail.com>
2026-08-18 17:39:06 +0530

    basic conditions

--- collage_student_table.sql
+++ collage_student_table.sql
@@ -12,6 +12,22 @@ VALUES
 ('Rahul', 20, 'rahul@gmail.com'),
 ('Priya', 21, 'priya@gmail.com'),
 ('amar', 21, 'amar@gmail.com'),
-('Amit', 19, 'amit@gmail.com');
+-- ('Amit', 19, 'amit@gmail.com');
 
 SELECT*  FROM students;
+
+
+SELECT NAME,EMAIL FROM students;
+
+
+SELECT * FROM students
+WHERE age>=20;
+
+SELECT * FROM students
+WHERE email = 'priya@gmail.com';
+
+SELECT * FROM students
+WHERE age>19 AND email='rahul@gmail.com';
+
+SELECT * FROM students
+WHERE age=19 OR age=21;
