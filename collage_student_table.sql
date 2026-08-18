f0a4fdc9e6095d30e046fb11379efa496ac98ef3
Lakshbethariya <Lakshbethariya03@gmail.com>
2026-08-18 17:35:01 +0530

    add sample data

--- collage_student_table.sql
+++ collage_student_table.sql
@@ -6,3 +6,12 @@ CREATE TABLE students (
     age INT,
     email VARCHAR(100)
 );
+
+INSERT INTO students (name, age, email)
+VALUES
+('Rahul', 20, 'rahul@gmail.com'),
+('Priya', 21, 'priya@gmail.com'),
+('amar', 21, 'amar@gmail.com'),
+('Amit', 19, 'amit@gmail.com');
+
+SELECT*  FROM students;
