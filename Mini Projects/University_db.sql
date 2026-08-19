96f21576bd6739d31c22bfb6c584483ce8d66a01
Lakshbethariya <Lakshbethariya03@gmail.com>
2026-08-19 20:00:08 +0530

    add department table

--- Mini Projects/University_db.sql
+++ Mini Projects/University_db.sql
@@ -5,3 +5,17 @@ CREATE TABLE departments (
 	department_id INT PRIMARY KEY AUTO_INCREMENT,
     department_name VARCHAR(100) NOT NULL
     );
+
+INSERT INTO departments (department_name)
+VALUES
+('Computer Science'),
+('Mechanical Engineering'),
+('Electrical Engineering'),
+('Civil Engineering'),
+('Information Technology'),
+('Electronics Engineering'),
+('Chemical Engineering'),
+('Biotechnology'),
+('Aerospace Engineering'),
+('Artificial Intelligence');
+
